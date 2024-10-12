e25a97782051dfedbfeee7878fe5ca11d7706246
mateidiana <mateidiana10@gmail.com>
2024-10-12 22:25:21 +0300

    ADDED ex 3

---
+++ Lab1/src/Ex3/Main.java
@@ -0,0 +1,23 @@
+package Ex3;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberProcessor np = new NumberProcessor();
+
+        int[] nr1 = new int[]{1, 3, 0, 0};
+        int[] nr2 = new int[]{8, 7, 0, 0};
+        System.out.println("Sum: " + Arrays.toString(np.sum(nr1, nr2)));
+
+        int[] nr3 = new int[]{8, 3, 0, 1};
+        int[] nr4 = new int[]{8, 3, 0, 0};
+        System.out.println("Sub: " + Arrays.toString(np.sub(nr3, nr4)));
+
+        int[] nr5 = new int[]{2, 3, 6, 0};
+        System.out.println("Multiply : " + Arrays.toString(np.multiply(nr5, 2)));
+
+        int[] nr6 = new int[]{8, 1};
+        System.out.println("Divide: " + Arrays.toString(np.divide(nr6, 9)));
+    }
+}
---
+++ Lab1/src/Ex3/NumberProcessor.java
@@ -0,0 +1,111 @@
+package Ex3;
+
+public class NumberProcessor {
+    public int[] sum(final int[] nr1, final int[] nr2) {
+        validateNumber(nr1);
+        validateNumber(nr2);
+        int[] result = new int[nr1.length];
+        int carry = 0;
+        for (int i = nr1.length - 1; i >= 0; i--) {
+            result[i] = (nr1[i] + nr2[i] + carry) % 10;
+            carry = (nr1[i] + nr2[i] + carry) / 10;
+        }
+        if (carry > 0) {
+            result = extend(result, carry);
+        }
+        return result;
+    }
+
+    public int[] extend(final int[] number, final int carry) {
+        int[] result = new int[number.length + 1];
+        result[0] = carry;
+        System.arraycopy(number, 0, result, 1, number.length);
+        validateNumber(result);
+        return result;
+    }
+
+    public int[] sub(final int[] nr1, final int[] nr2) {
+        validateNumber(nr1);
+        validateNumber(nr2);
+        int[] result = new int[nr1.length];
+        int carry = 0;
+        for (int i = nr1.length - 1; i >= 0; i--) {
+            if (nr1[i] >= nr2[i] + carry) {
+                result[i] = nr1[i] - nr2[i] - carry;
+                carry = 0;
+            } else {
+                result[i] = 10 + nr1[i] - nr2[i] - carry;
+                carry = 1;
+            }
+        }
+        if (result[0] == 0) {
+            result = makeSmaller(result);
+        }
+        return result;
+    }
+
+    public int[] makeSmaller(final int[] number) {
+        int j = 0;
+        while (number[j] == 0) {
+            j++;
+        }
+        int[] result = new int[number.length - j];
+        System.arraycopy(number, j, result, 0, result.length);
+        validateNumber(result);
+        return result;
+    }
+
+    public int[] multiply(final int[] number, final int multiplier) {
+        validateNumber(number);
+        validateDigit(multiplier);
+        int[] result = new int[number.length];
+        int carry = 0;
+        for (int i = number.length - 1; i >= 0; i--) {
+            int product = number[i] * multiplier + carry;
+            result[i] = product % 10;
+            carry = product / 10;
+        }
+        if (carry > 0) {
+            result = extend(result, carry);
+        }
+        return result;
+    }
+
+    public int[] divide(final int[] number, final int divisor) {
+        validateNumber(number);
+        validateDigit(divisor);
+        if (divisor == 0) {
+            throw new RuntimeException("Can't divide by 0.");
+        }
+
+        int[] result = new int[number.length];
+        int remainder = 0;
+        for (int i = 0; i < number.length; i++) {
+            int currentDigit = number[i] + remainder * 10;
+            result[i] = currentDigit / divisor;
+            remainder = currentDigit % divisor;
+        }
+        if (result[0] == 0) {
+            result = makeSmaller(result);
+        }
+        return result;
+    }
+
+    private void validateNumber(int[] number) {
+        if (number.length == 0) {
+            throw new RuntimeException("Number should not be null.");
+        }
+        if (number[0] == 0 && number.length > 1) {
+            throw new RuntimeException("Number can't start with 0.");
+        }
+        for (int digit : number) {
+            validateDigit(digit);
+        }
+    }
+
+    private void validateDigit(final int digit) {
+        if (digit > 9 || digit < 0) {
+            throw new RuntimeException("Invalid digit.");
+        }
+    }
+}
