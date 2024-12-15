ea709701b42640cf19112e1d9c30b200dcf745e6
mateidiana <mateidiana10@gmail.com>
2024-12-15 23:54:05 +0200

    added service for reading exam

--- Lab4/src/main/java/org/example/service/GrammarExamService.java
+++ Lab4/src/main/java/org/example/service/GrammarExamService.java
@@ -1,4 +1,14 @@
 package org.example.service;
 
 public class GrammarExamService {
+    //constructor
+    //getters
+    //take exam, handle answer
+    //show grammar exams results
+    //add result
+    //show all grammar exams
+    //show results of all students on all grammar exams of a teacher
+    //delete exam
+    //create/update exam
+    //sort students by avg grade on all exams
 }
--- Lab4/src/main/java/org/example/service/VocabularyExamService.java
+++ Lab4/src/main/java/org/example/service/VocabularyExamService.java
@@ -1,4 +1,14 @@
 package org.example.service;
 
 public class VocabularyExamService {
+    //constructor
+    //getters
+    //take exam, handle answer
+    //show exams results
+    //add result
+    //show all exams
+    //show results of all students on all exams of a teacher
+    //delete exam
+    //create/update exam
+    //filter students by highest
 }
